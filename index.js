1815a6a896199ba404d50519379125f2f26743e3
PabloFNK <pablo.fnk.foncas@gmail.com>
2019-06-14 18:30:26 +0200

    Add husky, prettier and lint-staged

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const ytdl = require('ytdl-core');
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
 const app = express();
 
 app.use(cors());
 
 app.listen(3000, () => {
-    console.log('Server Works !!! At port 3000');
+  console.log("Server Works !!! At port 3000");
+});
+
+app.get("/download", (req, res) => {
+  var URL = req.query.URL;
+  res.json({ url: URL });
 });
